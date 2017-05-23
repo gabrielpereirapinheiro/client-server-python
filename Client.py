a2334c1929747091f488fe6e998dab5b15d275c8
icevct <icevct@gmail.com>
2017-05-22 22:52:02 -0300

    client original

--- Client.py
+++ Client.py
@@ -36,26 +36,25 @@ def main():
 	serverName = '' 
 	serverPort = 12000
 	clientSocket = socket(AF_INET, SOCK_DGRAM)
-	message = raw_input('Insira uma mensagem: ')
+	message = raw_input('Digite a mensagem a ser enviada: ')
 	msg_size = len(message) # tamanho da string digitada
 
 	# Declaracao das variaveis de controle do GBN
-	window_size = 4 # o tamanho da janela eh variavel, precisa mudar o valor aqui
+	window_size = 3 # o tamanho da janela eh variavel, precisa mudar o valor aqui
 	seq_number = 0
 	window_base = 0
 	#se o tamanho da mensagem for menor que o tamanho da janela, o tamanho maximo da janela deve ser o tamanho da mensagem
 	if msg_size > window_size:
-		window_max = window_size - 1
+		window_max = window_size
 	else:
 		window_max = msg_size - 1
-	next_ack = 0 # o ack que o cliente espera
 	ack = 0 # o ack sempre comeca com 0, na medida que o algoritmo roda, que ele sera incrementado
 	nack = 0 # inicializando o nack como 0
 	queue_nack = [] # vai ser a fila que contera os NACKS recebidos
 	
 	#Variavel que vai 'destruir' a mensagem criada, ou seja, vai fazer com que nao seja entregue ao servidor, 
 	#para testar casos de perda
-	destroy_message = 5 #inicializa a variavel de destruir a mensagem como -1. Se quiser destruir alguma, 
+	destroy_message = 4 #inicializa a variavel de destruir a mensagem como -1. Se quiser destruir alguma, 
 						 #deve mudar aqui
 
 	message_list = message_assembler(message, msg_size) # vai criar a lista de mensagens que serao enviadas
@@ -64,49 +63,50 @@ def main():
 	#Enquanto o ACK nao for o valor do ultimo indice da mensagem, envia a mensagem
 	#Quando for, o loop para, e quer dizer que toda a mensagem foi enviada com sucesso
 	while ack != msg_size - 1:
-			#parte que incrementa o sequence number e envia o pacote
-			if seq_number == next_ack:
-				if seq_number == destroy_message:
-					destroy_message = -1
-					seq_number += 1
+			
+			if window_base == seq_number:
+				#colocar aqui a logica do timeout
+				print '\n'
+				queue_ack = [] # sempre inicializa a fila de acks
+				for i in range(window_base, window_max):
+					# se o i for igual a mensagem que quer ser destruida, continua o loop, assim n executa o
+					#envio da mensagem
+					if i == destroy_message:
+						destroy_message = -1
+					else:
+						seq_number += 1 # sequence number eh incrementado a cada vez que eh enviado um pacote
+						print 'Esta sendo enviado o pacote:', message_list[i]
+						clientSocket.sendto(message_list[i],(serverName, serverPort)) 
 
-				print 'Esta sendo enviado o pacote:', message_list[seq_number]
-				clientSocket.sendto(message_list[seq_number],(serverName, serverPort))
-				seq_number += 1 # sequence number eh incrementado a cada vez que eh enviado um pacote
+				print 'Pacotes de', window_base, 'ao', window_max-1, 'enviados'
 
 
-			#recebe a mensagem
 			received_message, serverAddress = clientSocket.recvfrom(2048)
-			print 'Mensagem recebida do servidor:', received_message, '\n'
+			print 'Mensagem recebida do servidor:', received_message
 			ack, rcv_validation = message_disassembler(received_message) # pega o ack recebido e se n teve erro
-			#se rcv_validation for -1, quer dizer que o pacote eh invalido, e coloca o ack na fila de nacks
+			
+			#se tiver tido erro, coloca ack na fila de nacks
 			if rcv_validation == -1:
 				queue_nack.append(ack) # colocar na fila qualquer ack que nao foi recebido
-			else:
-				next_ack = ack+1 #next ack recebe o proximo ack que deve receber
 
 			#se a fila de nack nao for vazia, quer dizer que houve erro na transmissao
 			if queue_nack != []:
 				print 'Foi perdido o pacote:', queue_nack[0]
-				print 'Reenviando o pacote', queue_nack[0]
+				print 'Reenviando'
 				nack = queue_nack[0] # vai receber o primeiro elemento da lista, ou seja, o primeiro da fila
 				queue_nack = []
-				next_ack = nack
+				ack = nack
 				seq_number = ack # sequence number tambem recebe o ack que foi perdido
-					
-	
-			#parte que vai deslizar a janela para o caso de: pacote ser invalido e valido
-			if rcv_validation == -1:
-				window_base = next_ack
-				window_max = next_ack + window_size
+				window_base = ack
+				window_max = ack + window_size
 			else:
-				window_max += (next_ack - window_base)
-				window_base = next_ack
+				window_max += (ack+1 - window_base)
+				window_base = ack + 1
 
 			if window_max > msg_size:
 				window_max = msg_size
 
-	print 'Mensagem', message, 'enviada com sucesso!'
+	print '\n\nMensagem', message, 'enviada com sucesso'
 	clientSocket.close()
 	
 
